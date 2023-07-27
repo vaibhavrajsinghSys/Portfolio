3e442ac3267c5d6e2c4e51d71d91e7ac5a0bfbfc
Kanishk Choraria <vaibhav.raj@sysquare.in>
2023-07-27 10:46:17 +0530

    feature/firebase_setup - firebase hosting and deployement is configured. - firebase project setup is complete for iOS, android, macOS and web.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -31,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page with firebase!'),
     );
   }
 }
