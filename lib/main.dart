9bf9e344fbbfa799fa80ed284b53714b2edee1ab
Kanishk Choraria <vaibhav.raj@sysquare.in>
2023-08-09 12:13:34 +0530

    feature/Application_dark_light_themes - small fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:portfolio/Utils/Themes.dart';
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // var firebaseDatabase = FirebaseFirestore.instance;
     return MaterialApp(
       title: 'Portfolio',
       theme: CustomThemes().lightThemes,
